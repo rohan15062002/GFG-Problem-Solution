3d337c6254e367f3bb502cd0de957ed4baafa3eb
rohan15062002 <78808303+rohan15062002@users.noreply.github.com>
2023-01-23 21:17:52 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Geeks And The String/geeks-and-the-string.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    string removePair(string s) {
+        // code here
+        stack<char> st;
+        int n = s.size();
+        
+        for(int i=0;i<n;i++){
+            if(st.empty()){
+                st.push(s[i]);
+            }
+            else{
+                char ch = st.top();
+                st.pop();
+                
+                if(ch==s[i]){
+                    continue;
+                }
+                else{
+                    st.push(ch);
+                    st.push(s[i]);
+                }
+            }
+        }
+        
+        string ans="";
+        if(st.empty()){
+            return "-1";
+        }
+        while(st.empty()!=true){
+            ans+=st.top();
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s; 
+        cin>>s;
+        
+        Solution obj;
+        string res = obj.removePair(s);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
